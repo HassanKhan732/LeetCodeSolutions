715f33953fd7802626fa77f784865923582333ac
HassanKhan732 <147940749+HassanKhan732@users.noreply.github.com>
2025-10-20 19:06:40 +0500

    solved

--- RomanToInteger.py
+++ RomanToInteger.py
@@ -1,7 +1,17 @@
-roman = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
-UserInput = input("Enter any Roman Value: ").capitalize()
-if UserInput in roman:
-    val = roman.values
-    print(val)
-else:
-    print("not a roman value")
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        result = 0
+        for i in range(len(s)):
+            current_value = roman[s[i]]
+            if i < len(s) - 1:
+                next_value = roman[s[i + 1]]
+                if current_value < next_value:
+                    result -= current_value
+                else:
+                    result += current_value
+            else:
+                result += current_value
+        return result
+
+        
